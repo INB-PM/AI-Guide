4ca6dc9e7275a2680d7f85bd096bb59f37383edb
PrinceDonda-INB <princedonda.inb@gmail.com>
2026-06-02 22:53:05 +0530

    Update layout.tsx , remove head

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,10 +31,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
-      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139550892247766" crossorigin="anonymous"></script>
-      </head>
       <body className="min-h-screen flex flex-col bg-zinc-950 text-zinc-100 bg-grid-pattern relative overflow-x-hidden selection:bg-indigo-500/30 selection:text-white">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139550892247766" crossorigin="anonymous"></script>
         {/* Glow ambient background effects wrapped to prevent stretching page height */}
         <div className="absolute inset-0 overflow-hidden pointer-events-none z-0">
           <div className="absolute top-[-10%] left-[-10%] w-[50%] h-[50%] rounded-full bg-indigo-500/5 blur-[150px] animate-pulse-glow" />
