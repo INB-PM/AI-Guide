77a344d90b96f4641238279c75ee7b0e8fd96dd1
MayankDobariya-INB <mayankdobariya.inb@gmail.com>
2026-06-30 21:24:59 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,17 @@ export default function RootLayout({
         </div>
 
         <Navbar />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139550892247766" crossorigin="anonymous"></script>
+        <!-- banner ad -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-9139550892247766"
+             data-ad-slot="1240686209"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         <main className="flex-1 flex flex-col relative z-10">{children}</main>
         <Footer />
       </body>
